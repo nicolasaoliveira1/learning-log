c547bf1be6acc9ee3711ad560aa710cfd1803e67
Nicolas <nicolasimbe2006@gmail.com>
2025-01-14 16:02:58 -0300

    adiciona a função de criar anotações em tópicos

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -35,3 +35,21 @@ def new_topic(request):
             return HttpResponseRedirect(reverse("topics"))
     context = {"form": form}
     return render(request, "learning_logs/new_topic.html", context)
+
+def new_entry(request, topic_id):
+    """Adiciona uma nova anotação em um tópico específico"""
+    topic = Topic.objects.get(id = topic_id)
+
+    if request.method != "POST":
+        #Nenhum dado foi enviado; cria form em branco
+        form = EntryForm()
+    else:
+        #Dados POST encontrados; processamento dados
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse("topic", args=[topic_id]))
+    context = {"topic": topic, "form": form}
+    return render(request, "learning_logs/new_entry.html", context)
