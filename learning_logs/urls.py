9d936f81a282531fe36614d26be13c0fada9ab4b
Nicolas <nicolasimbe2006@gmail.com>
2025-01-14 14:49:23 -0300

    adiciona links para acessar as paginas do site

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("topics", views.topics, name="topics"),
-    path("topic/<topic_id>/", views.topic, name="topic"),
+    path("topics/<topic_id>/", views.topic, name="topic"),
 ]
