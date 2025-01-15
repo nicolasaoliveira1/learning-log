11d759c0e6190dc1871d0973c5b6a098f03f921d
Nicolas <nicolasimbe2006@gmail.com>
2025-01-15 11:05:47 -0300

    Cria app users e adiciona função de login

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("learning_logs.urls")),
+    path("users/", include("users.urls")),
 ]
