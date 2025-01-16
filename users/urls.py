11abb375c5ed6ff2e83d8c4e5c6500b25a340e46
Nicolas <nicolasimbe2006@gmail.com>
2025-01-16 13:56:43 -0300

    cria função de registrar um usuario novo

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("login", auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path("logout", views.logout_view, name = "logout"),
+    path("register", views.register, name="register"),
 ]
